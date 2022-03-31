07d5ccf66c882565651dec95a35b73801a41a495
Katherine Alvarez <alvarez_katherine@outlook.com>
2022-03-30 21:36:41 -0600

    Update JavaApplication16.java
    desde visual

--- JavaApplication16/src/javaapplication16/JavaApplication16.java
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -17,6 +17,8 @@ public class JavaApplication16 {
     public static void main(String[] args) {
         System.out.println("HOLAAAAA ");
         System.out.println("prueba");
+        System.out.println("linea de kathy desde VS");
+    
     }
     
 }
