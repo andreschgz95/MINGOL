94c19702b491ac8a7215d9b7e9055ae15d995525
Karla-AguilarG <kbaguilar@uned.cr>
2022-03-30 21:04:36 -0600

    30/03/2021

---
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -0,0 +1,14 @@
+package ejemplo;
+
+/**
+ *
+ * @author Karla
+ */
+
+public class Ejemplo {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA A TODXS");
+    }
+    
+}
